1cc735fa75f96c5fa06676beac30c9690cc66d93
Saurav Rathore <saurav_rathore@epam.com>
2024-01-26 19:14:14 +0530

    Added Facade design pattern

---
+++ src/learn/java/structural/facade/FacadeDriver.java
@@ -0,0 +1,13 @@
+package learn.java.structural.facade;
+
+/*
+The Facade Pattern provides a unified interface to a set of interfaces in a subsystem. Facade defines a higher level interface that makes the subsystem easier to use.
+Use the Facade pattern when you need to have a limited but straightforward interface to a complex subsystem.
+
+Principle of Least Knowledge: talk only to your immediate friends
+
+*/
+
+
+public class FacadeDriver {
+}
