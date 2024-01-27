64c4bc910c036ad772a1239ccb4d6d1ee4d6012f
Saurav Rathore <saurav_rathore@epam.com>
2024-01-27 18:00:02 +0530

    Added Decorator JDK example

--- src/learn/java/structural/decorator/toppings/ToppingDecorator.java
+++ src/learn/java/structural/decorator/toppings/ToppingDecorator.java
@@ -8,6 +8,18 @@ Lets you attach new behaviors to objects by placing these objects inside special
 
 Use the Decorator pattern when you need to be able to assign extra behaviors to objects at runtime without breaking the code that uses these objects.
 
+Decorators have the same supertype as the objects they decorate.
+You can use one or more decorators to wrap an object.
+Given that the decorator has the same supertype as the object it decorates, we can  pass around a decorated object in place of the original (wrapped) object.
+The decorator adds its own behavior before and/or after delegating to the object it  decorates to do the rest of the job.
+Objects can be decorated at any time, so we can decorate objects dynamically at runtime with as many decorators as we like.
+
+Decorator Pattern solves Class Explosion problem
+
+The Decorator Pattern attaches additional responsibilities to an object dynamically. Decorators provide a flexible alternative to subclassing for extending functionality.
+
+JDK Examples - All implementations of Reader and Writer in java.io: BufferedReader, BufferedWriter, FileReader, FileWriter
+
 **/
 
 public abstract class ToppingDecorator extends BasePizza {
