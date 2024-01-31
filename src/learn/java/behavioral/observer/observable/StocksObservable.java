5b49a8c7311a543f2b91200ed8a66350c73bfcdc
Saurav Rathore <saurav_rathore@epam.com>
2024-01-31 20:00:45 +0530

    Added Observable and Strategy design patterns

--- src/learn/java/behavioral/observer/observable/StocksObservable.java
+++ src/learn/java/behavioral/observer/observable/StocksObservable.java
@@ -6,7 +6,9 @@ import learn.java.behavioral.observer.observer.NotificationAlertObserver;
 
 Observer Design Pattern: The Observer Pattern defines a one-to-many dependency between objects so that when one object changes state, all of its dependents are notified and updated automatically.
 
-
+JDK example -
+            java.util.Observer and java.util.Observable
+            java.beans.PropertyChangeListener
 
 
 
--- src/learn/java/behavioral/strategy/driveStrategy/DriveStrategy.java
+++ src/learn/java/behavioral/strategy/driveStrategy/DriveStrategy.java
@@ -1,5 +1,16 @@
 package learn.java.behavioral.strategy.driveStrategy;
 
+/*
+
+Strategy is a behavioral design pattern that lets you define a family of algorithms, put each of them into a separate class, and make their objects interchangeable.
+Essentially, the strategy pattern allows us to change the behavior of an algorithm at runtime.
+
+JDK examples - java.util.Comparator, java.util.concurrent.Executor
+
+
+*/
+
+
 public interface DriveStrategy {
     public void drive();
 }
