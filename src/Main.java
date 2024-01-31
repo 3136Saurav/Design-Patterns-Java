55b15325688261f24fd2e394ad43457a69355dcb
Saurav Rathore <saurav_rathore@epam.com>
2024-01-31 18:33:09 +0530

    Added Creational, Structural & Behavioral Design pattern definitions

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,13 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+/*
+
+CREATIONAL DESIGN PATTERN - Creational design patterns provide various object creation mechanisms, which increase flexibility and reuse of existing code.
+
+STRUCTURAL DESIGN PATTERN - Structural design patterns explain how to assemble objects and classes into larger structures, while keeping these structures flexible and efficient.
+
+BEHAVIORAL DESIGN PATTERN - Behavioral design patterns are concerned with algorithms and the assignment of responsibilities between objects.
+
+* */
