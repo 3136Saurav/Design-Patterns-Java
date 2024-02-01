c0efd8397f18cfd2881043d597bbe2d5bad31dd7
Saurav Rathore <saurav_rathore@epam.com>
2024-02-01 16:11:03 +0530

    Added Chain Of Responsibility jdk example

--- src/learn/java/behavioral/chainOfResponsibility/logger/LogProcessor.java
+++ src/learn/java/behavioral/chainOfResponsibility/logger/LogProcessor.java
@@ -1,5 +1,16 @@
 package learn.java.behavioral.chainOfResponsibility.logger;
 
+
+/*
+Chain of Responsibility is a behavioral design pattern that lets you pass requests along a chain of handlers. Upon receiving a request, each handler decides either to process the request or to pass it to the next handler in the chain.
+
+Use the Chain of Responsibility pattern when your program is expected to process different kinds of requests in various ways, but the exact types of requests and their sequences are unknown beforehand.
+
+Jdk examples - java.util.logging.Logger, javax.servlet.Filter
+
+*/
+
+
 public abstract class LogProcessor {
     public static int INFO = 1;
     public static int DEBUG = 2;
